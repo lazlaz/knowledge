4e8039c71a4599f1e1aedeefddef978e234a4fce
liuzhai <578091472@qq.com>
2020-10-15 16:08:42 +0800

    update 45

--- src/main/java/com/laz/knowledge/fortyfive/WeakReferenceTest.java
+++ src/main/java/com/laz/knowledge/fortyfive/WeakReferenceTest.java
@@ -5,8 +5,10 @@ import java.util.concurrent.TimeUnit;
 
 public class WeakReferenceTest {
 	public static void main(String[] args) throws InterruptedException {
-		WeakReference<Obj> weakReference = new WeakReference<>(new Obj());
+		Obj obj = new Obj();
+		WeakReference<Obj> weakReference = new WeakReference<>(obj);
 		System.out.println(weakReference.get() == null);// false
+		obj = null;
 		System.gc();
 		TimeUnit.SECONDS.sleep(1);// 暂停一秒钟
 		System.out.println(weakReference.get() == null);// true
