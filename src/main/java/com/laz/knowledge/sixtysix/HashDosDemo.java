cd796a4d02ac8a902cc0375dc0d06f0b7c3c633c
liuzhai <578091472@qq.com>
2020-11-16 14:39:09 +0800

    Hash Dos示例

--- src/main/java/com/laz/knowledge/sixtysix/HashDosDemo.java
+++ src/main/java/com/laz/knowledge/sixtysix/HashDosDemo.java
@@ -15,8 +15,8 @@ public class HashDosDemo {
 		System.out.println(new HashDosDemo().index());
 	}
 
+	private String jsonStr = "";
 	public String index() throws URISyntaxException {
-		String jsonStr = "";
 		try {
 			File file = new File(this.getClass().getResource("./javaHash.json").toURI());
 			FileReader fr = new FileReader(file);// 需要读取的文件路径
@@ -28,9 +28,21 @@ public class HashDosDemo {
 			System.out.println("指定文件不存在");// 处理异常
 		}
 
-		Map<String, Object> map = new HashMap<String, Object>();
-		map = JSON.parseObject(jsonStr, Map.class);//map中节点hash值一样，转换 成了红黑树
-		map.put("1", "1");
+
+		for (int i=0;i<20;i++) {
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					Map<String, Object> map = new HashMap<String, Object>();
+					map = JSON.parseObject(jsonStr, Map.class);//map中节点hash值一样，转换 成了红黑树
+//					for (int i=0;i<60000;i++) {
+//						map.put(i+"", i);
+//					}
+					//System.out.println(map);
+				}
+			}).start();
+		}
+	
 		return "Hash Collision ~";
 	}
 }
